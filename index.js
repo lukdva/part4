95525d111016da892bd1382e82bcae167c6885da
LukasD <lukas.dvarionis@gmail.com>
2022-06-28 19:11:27 +0300

    Using logger methods instead of console.log

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {PORT} = require('./utils/config')
-const {info, error} = require('./utils/logger');
+const {info} = require('./utils/logger');
 const app = require('./app');
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  info(`Server running on port ${PORT}`)
 })
