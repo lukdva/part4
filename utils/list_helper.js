58e85f30b87f16795642ff0c211b9e5e614da8fd
LukasD <lukas.dvarionis@gmail.com>
2022-06-29 14:30:51 +0300

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,15 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce(total, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (blog1, blog2) => {
+    return blog1.likes >= blog2.likes ? blog1:blog2;
+  }
+  return blogs.length === 0 ? {} : blogs.reduce(reducer)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
