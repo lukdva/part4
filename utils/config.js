30b8a0c2267ffbc55eb598367cf46b6f15f698e1
LukasD <lukas.dvarionis@gmail.com>
2022-07-08 22:52:34 +0300

    Wrote a test for /api/blogs GET reuest to test it returns correct data Changed config.js to use separate DB for testing purposes

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.NODE_ENV === 'test'
+? process.env.MONGO_TEST_URI 
+: process.env.MONGO_URI;
 
 module.exports ={
     PORT,
