35e656c53d8eae0aa120084bce8794b84d7ed258
LukasD <lukas.dvarionis@gmail.com>
2022-07-08 22:47:40 +0300

    Added NODE_ENV variables to npm scripts Installed supertest and express-assync-errors Using express async errors in the app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('express-async-errors');
 const {MONGO_URI} = require('./utils/config')
 const mongoose = require('mongoose')
 const cors = require('cors')
