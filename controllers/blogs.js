b35b98fd153c1a0fcfd4c776ff5782b9e768ddd6
LukasD <lukas.dvarionis@gmail.com>
2022-07-10 20:45:56 +0300

    Added few rest methods for testing purposes to blogs.rest Created put method for single blog Added blog model validation so that likes can be positive number or 0 Created tests for PUT method (Increasing, decreasing likes, setting likes to negative ammount, editing deleted blog) Grouped old tests with describe bocks

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,10 +18,17 @@ blogsRouter.get('/', async (request, response, next) => {
   })
 
   blogsRouter.delete('/:id', async (request, response) => {
-    const result = await Blog.deleteOne({_id:request.id});
+    const result = await Blog.findByIdAndDelete(request.params.id);
     if(result.acknowledged === true && result.deletedCount === 0)
       throw new Error('not found');
     response.status(204).send();
   })
 
+  blogsRouter.put('/:id', async (request, response) => {
+    if (request.body.likes < 0 )
+      throw new Error('bad request')
+    const result = await Blog.findByIdAndUpdate({_id:request.params.id}, {...request.body}, {new: true, runValidators: true});
+    console.log('RESULT',result);
+    response.status(200).json(result.body)
+  })
   module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -12,72 +12,77 @@ const api = supertest(app)
     const promiseArray = blogArray.map(blog => blog.save())
     await Promise.all(promiseArray);
   })
-test('Blogs are returned in JSON format and a correct number of blogs', async () => {
-    const result = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-    
-    expect(result.body.length).toEqual(helper.initialBlogs.length)
-})
+  describe('GET method tests', () =>{
 
-test('Blog post identifiers are named id', async () => {
-    const result = await api
-    .get('/api/blogs')
-    .expect(200)
-
-    console.log(result.body);
-    result.body.forEach(blog => {
-      expect(blog.id).toBeDefined();
-    })
-})
-
-test('Blog is created with POST method', async () => {
-  await api
-  .post('/api/blogs')
-  .send(helper.blogToAdd)
-  .set('Accept', 'application/json')
-  .expect('Content-Type', /json/)
-  .expect(201);
-
-  const result = await api.get('/api/blogs');
-  const titles = result.body.map(r => r.title);
-  expect(result.body).toHaveLength(helper.initialBlogs.length + 1)
-  expect(titles).toContain(helper.blogToAdd.title)
-
-  const filter = {title: helper.blogToAdd.title};
-  const toObjectOptions = {transform : (doc, ret) => { delete ret._id; delete ret.__v}};
-  const processedBlogs = await helper.getBlogsInDB(filter, toObjectOptions);
-  console.log(processedBlogs);
-  expect(processedBlogs[0]).toEqual(helper.blogToAdd);
+    test('Blogs are returned in JSON format and a correct number of blogs', async () => {
+      const result = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      
+      expect(result.body.length).toEqual(helper.initialBlogs.length)
+  })
   
+  test('Blog post identifiers are named id', async () => {
+      const result = await api
+      .get('/api/blogs')
+      .expect(200)
+  
+      result.body.forEach(blog => {
+        expect(blog.id).toBeDefined();
+      })
+  })
+  })
+
+describe('POST method tests', () => {
+  test('Blog is created with POST method', async () => {
+    await api
+    .post('/api/blogs')
+    .send(helper.blogToAdd)
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(201);
+  
+    const result = await api.get('/api/blogs');
+    const titles = result.body.map(r => r.title);
+    expect(result.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(titles).toContain(helper.blogToAdd.title)
+  
+    const filter = {title: helper.blogToAdd.title};
+    const toObjectOptions = {transform : (doc, ret) => { delete ret._id; delete ret.__v}};
+    const processedBlogs = await helper.getBlogsInDB(filter, toObjectOptions);
+    console.log(processedBlogs);
+    expect(processedBlogs[0]).toEqual(helper.blogToAdd);
+    
+  })
+  
+  test('No likes prop defaults to 0', async() => {
+    const result = await api
+    .post('/api/blogs')
+    .send(helper.blogNoLikesProp)
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(201);
+  
+    expect(result.body.likes).toBeDefined();
+  
+    const filter = {title: helper.blogNoLikesProp.title};
+    const processedBlogs = await helper.getBlogsInDB(filter);
+    expect(processedBlogs[0].likes).toBe(0);
+  })
+  
+  test('No URL and Title returns 400 status', async() => {
+    const result = await api
+    .post('/api/blogs')
+    .send(helper.blogNoUrlTitle)
+    .set('Accept', 'application/json')
+    .expect(400);
+  
+    expect(result.body.error).toBeDefined();
+    expect(result.body.error).toBe('bad request')
+  })
 })
 
-test('No likes prop defaults to 0', async() => {
-  const result = await api
-  .post('/api/blogs')
-  .send(helper.blogNoLikesProp)
-  .set('Accept', 'application/json')
-  .expect('Content-Type', /json/)
-  .expect(201);
-
-  expect(result.body.likes).toBeDefined();
-
-  const filter = {title: helper.blogNoLikesProp.title};
-  const processedBlogs = await helper.getBlogsInDB(filter);
-  expect(processedBlogs[0].likes).toBe(0);
-})
-
-test('No URL and Title returns 400 status', async() => {
-  const result = await api
-  .post('/api/blogs')
-  .send(helper.blogNoUrlTitle)
-  .set('Accept', 'application/json')
-  .expect(400);
-
-  expect(result.body.error).toBeDefined();
-  expect(result.body.error).toBe('bad request')
-})
 
 describe('Deleting blog', () => {
 
@@ -93,11 +98,64 @@ describe('Deleting blog', () => {
     const result = await api
     .delete(`/api/blogs/${nonExistantId}`)
     .expect(404)
-
-    expect(result.body.error).toBeDefined();
-    expect(result.body.error).toBe('not found')
   })
 })
+
+describe('Updating blog', () => {
+
+  test('Increasing blog likes by 1', async () => {
+  const blogs = await helper.getBlogsInDB();
+  const blogToUpdate = blogs[0];
+  const result = await api
+  .put(`/api/blogs/${blogToUpdate._id}`)
+  .send({...blogToUpdate, likes:blogToUpdate.likes + 1})
+  .expect(200)
+
+  const filter = {_id: blogToUpdate._id};
+  const updatedBlogs = await helper.getBlogsInDB(filter);
+  expect(updatedBlogs[0].likes).toBe(blogToUpdate.likes + 1)
+  })
+
+  test('Try to update already removed blog', async () => {
+    const blogs = await helper.getBlogsInDB();
+    const blogToUpdate = blogs[0];
+    await Blog.findByIdAndDelete(blogToUpdate._id)
+    const result = await api
+    .put(`/api/blogs/${blogToUpdate._id}`)
+    .send({...blogToUpdate, likes:blogToUpdate.likes + 1})
+    .expect(404)
+     })
+  
+  test('Decreasing blog likes by 1', async () => {
+    const blogs = await helper.getBlogsInDB();
+    const blogToUpdate = blogs[0];
+    const result = await api
+    .put(`/api/blogs/${blogToUpdate._id}`)
+    .send({...blogToUpdate, likes:blogToUpdate.likes - 1})
+    .expect(200)
+
+    const filter = {_id: blogToUpdate._id};
+    const updatedBlogs = await helper.getBlogsInDB(filter);
+    expect(updatedBlogs[0].likes).toBe(blogToUpdate.likes - 1)
+  })
+
+  test('Setting likes to negative ammount', async () => {
+    const blogs = await helper.getBlogsInDB();
+    const blogToUpdate = blogs[0];
+    const result = await api
+    .put(`/api/blogs/${blogToUpdate._id}`)
+    .send({...blogToUpdate, likes: -1})
+    .expect(400)
+    
+    expect(result.body.error).toBeDefined();
+    expect(result.body.error).toBe('bad request');
+
+    const filter = {_id: blogToUpdate._id};
+    const updatedBlogs = await helper.getBlogsInDB(filter);
+    expect(updatedBlogs[0].likes).toBe(blogToUpdate.likes)
+  })
+})
+
 afterAll(() => {
     mongoose.connection.close();
 });
